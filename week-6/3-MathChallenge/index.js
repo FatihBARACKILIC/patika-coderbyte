f0c483cc862e0710de0bd9476cc4c520f0b75d61
Fatih BARAÇKILIÇ <fatihb469@gmail.com>
2022-10-26 00:21:34 +0300

    MathChallenge

---
+++ week-6/3-MathChallenge/index.js
@@ -0,0 +1,8 @@
+function MathChallenge(str) {
+  return str
+}
+
+// keep this function call here
+console.log(MathChallenge("(1x)(2x^-2+1)"))
+console.log(MathChallenge("(-1x^3)(3x^3+2)"))
+console.log(MathChallenge("(2x^2+4)(6x^3+3)"))
